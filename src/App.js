b08adcabd6f0f926678151d60b6c959b18750487
Udacity Workspace <samadarshad@hotmail.co.uk>
2021-01-26 20:55:37 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
-
+import AssociateABText from './AssociateABText'
 /*
 Use React and the data below to display a list of users alongside their favorite movies.
 
@@ -106,6 +106,7 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<AssociateABText associationMap={profiles} A={users} B={movies} text="'s favorite movie is " />
       </div>
     );
   }
